146ea51d1a62c02e92144760465ad2e2523d8e37
Ramsey Dow <yesmar@gmail.com>
2019-08-23 16:54:51 -0700

    Wrap long comment on math.BigFloatToBigInt

--- math/float.go
+++ math/float.go
@@ -7,7 +7,11 @@ import (
 	"math/big"
 )
 
-// BigFloatToBigInt converts big.Float values to big.Int values. This function is required because big.NewInt() simply casts to uint64, and is thus bounded by that type's maximum size, i.e. math.MaxUint64 or 18446744073709551615. To get around this, we use the Int() method on big.Float to directly store large values into a big.Int.
+// BigFloatToBigInt converts big.Float values to big.Int values. This
+// function is required because big.NewInt() simply casts to uint64, and
+// is thus bounded by that type's maximum size, i.e. math.MaxUint64 or
+// 18446744073709551615. To get around this, we use the Int() method on
+// big.Float to directly store large values into a big.Int.
 func BigFloatToBigInt(bf *big.Float) (*big.Int, error) {
 	if bf == nil {
 		return nil, errors.New("nil big.Float")
